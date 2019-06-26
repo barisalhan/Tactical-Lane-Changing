ad915d8b2795d17d7a0141c9e97852f249fb5783
barisalhan <barisalhan003@gmail.com>
2019-06-26 18:02:22 +0300

    first commit

---
+++ Vehicle/VehicleDynamics/VehiclePhysicalProperties.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 26 17:19:57 2019
+
+@author: Baris ALHAN
+"""
+class VehiclePhysicalProperties:
+    """
+        The class holds the properties of a vehicle.
+    """
+    
+    def __init__(self,width=45,height=25):
+        self._width = width
+        self._height = height
+
+
+'''
+if __name__ == "__main__": 
+    my_prop = VehiclePhysicalProperties(4) 
+    my_prop2 = VehiclePhysicalProperties() 
+    print(my_prop2._width)
+'''
---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 06/26/2019
+
+@author: Baris ALHAN
+"""
+
+import os
+import time
+import random
+import pygame
+
+import numpy as np
+
+from pygame.locals import *
+
+def main():
+    
+    print('inside the main')
+
+if __name__ == "__main__": 
+    main()
+    
